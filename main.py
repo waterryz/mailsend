fb71eed9a2af96a9ecaa8a21acd2a2166b724418
waterryz <alexyss.waterry@icloud.com>
2025-12-27 00:10:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
 from typing import Dict, Any
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
