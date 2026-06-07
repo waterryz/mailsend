13822bc2206bb2916427192561eba6133307e2d4
waterryz <alexyss.waterry@icloud.com>
2026-06-07 22:58:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],          # потом можно сузить до домена
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
